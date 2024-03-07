873ed5aea20254053b8e17d1f7b3b5f13ef2406f
Willard Washington Ford <willardwashingtonford@gmail.com>
2024-03-07 01:04:01 -0800

    Upload Skeleton Files

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup, find_packages
+
+# version-keeping code based on pybedtools
+curdir = os.path.abspath(os.path.dirname(__file__))
+MAJ = 0
+MIN = 0
+REV = 0
+VERSION = '%d.%d.%d' % (MAJ, MIN, REV)
+with open(os.path.join(curdir, 'wf-admixture/version.py'), 'w') as fout:
+        fout.write(
+            "\n".join(["",
+                       "# THIS FILE IS GENERATED FROM SETUP.PY",
+                       "version = '{version}'",
+                       "__version__ = version"]).format(version=VERSION)
+        )
+
+setup(
+    name='wf-admixture',
+    version=VERSION,
+    description='CSE284 Demo Project',
+    author='Willard Ford',
+    author_email='wwford@ucsd.edu',
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "wf-admixture=wf-admixture.wf-admixture:main"
+        ],
+    },
+)
---
+++ wf-admixture/wf-admixture.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+"""
+Command-line script to perform admixture algorithm of individuals given Plink input
+
+Similar to ADMIXTURE
+"""
+
+import argparse
+from . import utils as utils
+import os
+import sys
+import time
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="",
+        description=""
+    )
+
+    # Inputs
+    parser.add_argument("bed",\
+                        help="A bed file.",
+                        metavar="BED", type = str)
+    
+    parser.add_argument("k", help="An integer representing the number of populations "\
+                        "to divide your dataset into.",
+                        metavar="K", type = int)
+    
+    # Output
+    parser.add_argument("-o", "--output", help="PREFIX will send your outputs to "\
+                        "PREFIX.Q and PREFIX.P and create a PREFIX.metrics file. " \
+                        "Default: stdout", metavar="PREFIX", type = str, required=False)
+
+    # Ideas:
+    #   
+
+    # Parse args
+    args = parser.parse_args()
+
+    # Set up output file
+    if args.output == None:
+        metrics = True
+        outf = sys.stdout
+    else: 
+        outf = open(args.output, "w")
+        metrics = False
+
+    print("Congratulations! You loaded v0.0.0")
+
+if __name__ == '__main__':
+    main()
